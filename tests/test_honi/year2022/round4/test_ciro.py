f3962c887c77ed4a0c1718ebc04e89c1fefb29d2
amamic1803 <antoniomamic007@gmail.com>
2025-09-14 01:21:35 +0200

    project restructure

---
+++ tests/test_honi/year2022/round4/test_ciro.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+from tests.test_honi.utils import HoniTest
+import honi.year2022.round4.ciro as script
+
+
+class TestScript(HoniTest):
+    def test_script(self):
+        self.run_io_tests(script, os.path.join(os.path.dirname(__file__), "test-data", "ciro"))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_honi/year2022/round5/test_kalendar.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+from tests.test_honi.utils import HoniTest
+import honi.year2022.round5.kalendar as script
+
+
+class TestScript(HoniTest):
+    def test_script(self):
+        self.run_io_tests(script, os.path.join(os.path.dirname(__file__), "test-data", "kalendar"))
+
+
+if __name__ == '__main__':
+    unittest.main()
